36007777ab126e1991d2e3a344f97d99c4b13dc3
Zhang Hai <dreaming.in.code.zh@gmail.com>
2017-03-12 23:08:01 +0800

    nx510j: Fix mkbootimg paramter substitution.

--- board/kernel.mk
+++ board/kernel.mk
@@ -11,7 +11,7 @@ TARGET_KERNEL_ARCH := arm64
 BOARD_KERNEL_CMDLINE := console=ttyHSL0,115200,n8 androidboot.hardware=qcom user_debug=31 msm_rtb.filter=0x37 ehci-hcd.park=3 lpm_levels.sleep_disabled=1 boot_cpus=0-5
 BOARD_KERNEL_IMAGE_NAME := Image.gz-dtb
 TARGET_KERNEL_APPEND_DTB := true
-BOARD_MKBOOTIMG_ARGS := --ramdisk_offset BOARD_RAMDISK_OFFSET --tags_offset BOARD_KERNEL_TAGS_OFFSET
+BOARD_MKBOOTIMG_ARGS := --ramdisk_offset $(BOARD_RAMDISK_OFFSET) --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
 
 ENABLE_CPUSETS := true
 
