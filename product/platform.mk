799192b7b90ebeeaf3bbdb67bfd3b4e53beee882
WouldChar <812303785@qq.com>
2017-04-19 23:23:56 +0800

    nx510j: Enable sdcardfs

--- product/platform.mk
+++ product/platform.mk
@@ -3,6 +3,9 @@ PRODUCT_PACKAGES += \
     init.qcom.rc \
     ueventd.qcom.rc
 
+# Storage
+ro.sys.sdcardfs=true
+
 # These are the hardware-specific features
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml
