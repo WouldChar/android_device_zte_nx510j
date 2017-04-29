3bc7176ffd6d7862b08274c63a3b8cba409aa481
WouldChar <wouldchar@gmail.com>
2017-04-29 18:36:33 +0800

    Update TWRP device tree

--- lineage.mk
+++ lineage.mk
@@ -16,14 +16,14 @@
 TARGET_SCREEN_HEIGHT := 1080
 TARGET_SCREEN_WIDTH := 1920
 
-# Inherit 64-bit configs
-$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, vendor/cm/config/common.mk)
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+# Inherit from the common Open Source product configuration
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base_telephony.mk)
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Charger
+PRODUCT_PACKAGES += charger charger_res_images \
+    init.recovery.qcom.rc
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := lineage_nx510j
