3fc66a9bdd6bfd7722457a338010cddc41ab4f0b
WouldChar <wouldchar@gmail.com>
2017-05-01 00:49:45 +0800

    nx510j: Add Cypress TMA568

--- rootdir/etc/init.tp.sh
+++ rootdir/etc/init.tp.sh
@@ -33,3 +33,47 @@ if [ -e "/sys/bus/i2c/devices/2-005d/keypad_enable" ]; then
 	chown system:system /sys/bus/i2c/devices/2-005d/keypad_enable
 	ln -s /sys/bus/i2c/devices/2-005d/keypad_enable /data/tp/keypad_enable
 fi
+
+# ==============================================================================
+# Cypress TMA568
+
+if [ -e "/sys/bus/i2c/devices/2-0024/ic_ver" ]; then
+	ln -s /sys/bus/i2c/devices/2-0024/ic_ver /data/tp/ic_ver
+fi
+
+if [ -e "/sys/bus/i2c/devices/2-0024/hw_reset" ]; then
+	ln -s /sys/bus/i2c/devices/2-0024/hw_reset /data/tp/reset
+	echo 1 > /sys/bus/i2c/devices/2-0024/hw_reset
+fi
+
+if [ -e "/sys/bus/i2c/devices/2-0024/ic_detect" ]; then
+	ln -s /sys/bus/i2c/devices/2-0024/ic_detect /data/tp/ic_detect
+fi
+
+if [ -e "/sys/bus/i2c/devices/2-0024/manual_cali" ]; then
+	ln -s /sys/bus/i2c/devices/2-0024/manual_cali /data/tp/manual_cali
+fi
+
+if [ -e "/sys/bus/i2c/devices/2-0024/fw_upgrade_flag" ]; then
+	ln -s /sys/bus/i2c/devices/2-0024/fw_upgrade_flag /data/tp/fw_upgrade_flag
+fi
+
+if [ -e "/sys/bus/i2c/devices/2-0024/easy_wakeup_gesture" ]; then
+	chown system:system /sys/bus/i2c/devices/2-0024/easy_wakeup_gesture
+	ln -s /sys/bus/i2c/devices/2-0024/easy_wakeup_gesture /data/tp/easy_wakeup_gesture
+fi
+
+if [ -e "/sys/bus/i2c/devices/2-0024/keypad_enable" ]; then
+	chown system:system /sys/bus/i2c/devices/2-0024/keypad_enable
+	ln -s /sys/bus/i2c/devices/2-0024/easy_wakeup_gesture /data/tp/keypad_enable
+fi
+
+if [ -e "/sys/bus/i2c/devices/2-0024/touch_mode" ]; then
+	chown system:system /sys/bus/i2c/devices/2-0024/touch_mode
+	ln -s /sys/bus/i2c/devices/2-0024/touch_mode /data/tp/touch_mode
+fi
+
+if [ -e "/sys/bus/i2c/devices/2-0024/hall_mode" ]; then
+	chown system:system /sys/bus/i2c/devices/2-0024/hall_mode
+	ln -s /sys/bus/i2c/devices/2-0024/hall_mode /data/tp/hall_mode
+fi
