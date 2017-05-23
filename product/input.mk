b7c95f976ec2d5229fe7c71649f9ce63ef0898ef
WouldChar <wouldchar@gmail.com>
2017-05-23 23:54:39 +0800

    nx510j: Cleanup keylayout

--- product/input.mk
+++ product/input.mk
@@ -1,10 +1,7 @@
 # Keylayout
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/keylayout/cyttsp5_btn.kl:system/usr/keylayout/cyttsp5_btn.kl \
     $(LOCAL_PATH)/keylayout/gpio-keys.kl:system/usr/keylayout/gpio-keys.kl \
-    $(LOCAL_PATH)/keylayout/cyttsp5_mt.kl:system/usr/keylayout/cyttsp5_mt.kl \
     $(LOCAL_PATH)/keylayout/qpnp_pon.kl:system/usr/keylayout/qpnp_pon.kl \
-    $(LOCAL_PATH)/keylayout/goodix-ts.kl:system/usr/keylayout/goodix-ts.kl \
     $(LOCAL_PATH)/keylayout/Generic.kl:system/usr/keylayout/Generic.kl
 
 PRODUCT_COPY_FILES += \
